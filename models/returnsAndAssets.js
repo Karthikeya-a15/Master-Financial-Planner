1876d0f5d8ed1a4f4416d2a1ad6182509427de87
Karthikeya <karthikeya.arvapalli@darwinbox.io>
2025-02-21 12:02:16 +0530

    ram Schema default values changed

--- models/returnsAndAssets.js
+++ models/returnsAndAssets.js
@@ -11,10 +11,38 @@ const commonSchema = {
 
 
 const ramSchema = new mongoose.Schema({
-        expectedReturns : commonSchema,
-        shortTerm : commonSchema,
-        mediumTerm : commonSchema,
-        longTerm : commonSchema
+        expectedReturns : {
+            domesticEquity: { type: Number, default: 12 },
+            usEquity: { type: Number, default: 12 },
+            debt: { type: Number, default: 6 },
+            gold: { type: Number, default: 6 },
+            crypto: { type: Number, default: 20 },
+            realEstate: { type: Number, default: 10 },
+        },
+        shortTerm : {
+            domesticEquity: { type: Number, default: 0 },
+            usEquity: { type: Number, default: 0 },
+            debt: { type: Number, default: 100 },
+            gold: { type: Number, default: 0 },
+            crypto: { type: Number, default: 0 },
+            realEstate: { type: Number, default: 0 },
+        },
+        mediumTerm : {
+            domesticEquity: { type: Number, default: 40 },
+            usEquity: { type: Number, default: 0 },
+            debt: { type: Number, default: 50 },
+            gold: { type: Number, default: 10 },
+            crypto: { type: Number, default: 0 },
+            realEstate: { type: Number, default: 0 },
+        },
+        longTerm : {
+            domesticEquity: { type: Number, default: 60 },
+            usEquity: { type: Number, default: 10 },
+            debt: { type: Number, default: 15 },
+            gold: { type: Number, default: 5 },
+            crypto: { type: Number, default: 5 },
+            realEstate: { type: Number, default: 5 },
+        }
 })
 
 const RAM = mongoose.model('ReturnsAndAssets', ramSchema);
